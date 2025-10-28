a08ef1f9e0599a86b88ca27b089a865970cf78fd
Green <greenguidertpproject@gmail.com>
2025-10-28 19:25:44 +0530

    helloworld.java updated

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,7 +1,7 @@
 package com.example;
 public class HelloWorld {
     public static String greet() {
-        return "Hello, Travis CI with Maven update!";
+        return "Hello, Travis CI with Maven updated!";
     }
 
     public static void main(String[] args) {
